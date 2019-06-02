51ee57dea9ee90cc7cf4738b4370caab6c4229fe
hecro459 <hectorrd@kth.se>
2019-06-02 16:21:21 +0200

    Add logging, and extra functions for matrix operations on Spark

--- spark.py
+++ spark.py
@@ -19,8 +19,19 @@ from pyspark.sql import SQLContext
 from pyspark import sql
 sqlContext = sql.SQLContext(sc)
 
-from pyspark import SparkConf
-logFile = "logFile.txt"
+#Set up loggin file with all the performance info we need
+#1) You need to create /conf directory under $SPARK_HOME
+#2) 
+#from pyspark import SparkConf
+#log4j = sc._jvm.org.apache.log4j
+#conf = sc.getConf()
+#app_id = conf.get('spark.app.id')
+#app_name = conf.get('spark.app.name')
+#message_prefix = '<' + app_name + ' ' + app_id + '>'
+#logger = log4j.LogManager.getLogger(message_prefix)
+
+sc.setLogLevel("INFO")
+
 
 #import scipy as sc
 import numpy as np
@@ -48,16 +59,53 @@ def do_spark():
     
     return "{}".format(v.norm(2))
 
-#print(do_spark())
 
-def do_spark_distributed():
-    print "This is example for distributed Matrix operations"
-    mat = IndexedRowMatrix(sc.parallelize([(0, (0, 1)), (1, (2, 3))]))
-    G = mat.computeGramianMatrix()
-    return "{}".format(G.numCols)
+#Calculates the matrix M^TM
+#M is an IndexedRowMatrix of size (m x n)
+def do_spark_Gramian(M):  
+    G = M.computeGramianMatrix()
+    return "Gramian matrix calculation complete"
+    
+
+#Calculates the SVD decomposition of M
+#M is an IndexedRowMatrix of size (m x n)
+#k is the number of singular values 1 <= k <= m
+#u is logical indicating if matrix U is returned   
+def do_spark_SVD(M,k,u):
+    svd = M.computeSVD(k,u)
+    return "SVD decomposition complete"
+    
+#Calculates the cosine similarities between the columns of M
+#M is an IndexedRowMatrix of size (m x n)
+def do_spark_cosine_sim(M):
+    cs = M.columnSimilarities()
+    return "Column similarities calculated"
 
 
+#Function for convert numpy matrix to Indexed Spark (Distributable Matrix)
+def indexed_matrix_from_numpy(M):
+    t = tuple(map(tuple, M))
+    t2 = range(len(t))
+    l = list(t)
+    for i in t2:
+        l[i]=tuple((i,l[i]))
+    idxM = IndexedRowMatrix(sc.parallelize(l))    
+    return idxM
 
 
+#Testing the functions defined above
+mat = IndexedRowMatrix(sc.parallelize([(0, (0, 1)), (1, (2, 3)), (2,(4,5))]))
+do_spark_Gramian(mat)
+do_spark_SVD(mat,2,"false")
+do_spark_cosine_sim(mat)
 
+
+#Bigger matrices
+n = 1000
+s1 = np.random.normal(0, 1, n*n)
+M = np.reshape(s1,(n,n))
+mat = indexed_matrix_from_numpy(M)
+do_spark_Gramian(mat)
+do_spark_SVD(mat,n,"false")
+do_spark_cosine_sim(mat)
 
